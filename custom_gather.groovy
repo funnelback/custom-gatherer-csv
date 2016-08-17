fe2cd1138a9599531471062c652898ddc44de63d
Peter Levan <plevan@peter-levans-macbook-pro.local>
2016-08-17 14:15:13 +1000

    PL: Various improvements

--- custom_gather.groovy
+++ custom_gather.groovy
@@ -12,46 +12,74 @@ import static org.apache.commons.csv.CSVFormat.*
 // Create a configuration object to read collection.cfg
 def config = new NoOptionsConfig(new File(args[0]), args[1]);
 
+if (config.value("csv.sourceurl") == null) {
+        throw new RuntimeException("Error: CSV source URL is not defined");
+}
+
 // Create a Store instance to store gathered data
 def store = new XmlStoreFactory(config).newStore();
 
-// set the fileformat based on supported types.  see https://commons.apache.org/proper/commons-csv/archives/1.0/apidocs/org/apache/commons/csv/CSVFormat.html
-def format = config.value("csv.format")
+// define default values
+def format = "csv";
+// define the available fileformat based on supported types.  see https://commons.apache.org/proper/commons-csv/archives/1.0/apidocs/org/apache/commons/csv/CSVFormat.html
 def csvFormat = ["csv":DEFAULT,"xls":EXCEL,"rfc4180":RFC4180,"tsv":TDF,"mysql":MYSQL]
-def csvEncoding = config.value("csv.encoding")
+def csvEncoding = "UTF-8"
+def csvHeader = "true";
+def csvDebug = false;
+
+// read config from collection.cfg and override defaults
+if (config.value("csv.format") != null) {format = config.value("csv.format")}
+if (config.value("csv.header") != null) {csvHeader = config.value("csv.header")}
+if (config.value("csv.encoding") != null) {csvEncoding = config.value("csv.encoding")}
+if (config.value("csv.format") != null) {format = config.value("csv.format")}
+if (config.value("csv.debug") != null) {csvDebug = config.value("csv.debug")}
 
 // Open the XML store
 store.open()
 
 // Fetch the CSV file and convert it to XML
-def csvText = new URL(config.value("csv.sourceurl")).getText(csvEncoding) 
-// Uncomment to see source data in update log
-//println csvText
+def csvText = new URL(config.value("csv.sourceurl")).getText(csvEncoding)
+// remove blank lines
+csvText = csvText.replaceAll("[\n\r]+","\n")
+
+if (csvDebug) {println csvText}
+
 CSVParser csv = null
-if (Boolean.parseBoolean(config.value("csv.header"))) { 
+if (Boolean.parseBoolean(csvHeader)) {
     // use the header row to define fields
-	csv = CSVParser.parse(csvText, csvFormat[format].withHeader())
+        csv = CSVParser.parse(csvText, csvFormat[format].withHeader())
 }
-else { 
-	// use field definitions
-	csv = CSVParser.parse(csvText, csvFormat[format].withHeader(config.value("csv.header.custom").split(",")))
+else {
+        if (config.value("csv.header.custom") != null) {
+        // use field definitions
+                csv = CSVParser.parse(csvText, csvFormat[format].withHeader(config.value("csv.header.custom").split(",")))
+        }
+        else {
+                csv = CSVParser.parse(csvText, csvFormat[format])
+        }
 }
 def i=0;
 for (record in csv.iterator()) {
+    if ((i % 100) == 0)
+    {
+        config.setProgressMessage("Processed "+i+" records");
+    }
     def fields = record.toMap();
-	def xmlString = new StringWriter();
-	def xmlRecord = new groovy.xml.MarkupBuilder(xmlString);
-	xmlRecord.item() {
-		fields.each() {key, value ->
-			// replace non-word chars with _ to avoid illegal XML field names
-			"${key.replaceAll(/\W/, '_')}""${value}"
-		}
-	}
-// Uncomment to see stored XML data in update log
-//    println "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"+xmlString
+        def xmlString = new StringWriter();
+        def xmlRecord = new groovy.xml.MarkupBuilder(xmlString);
+
+        xmlRecord.item() {
+                fields.each() {key, value ->
+                        // replace non-word chars with _ to avoid illegal XML field names and strip whitespace from values
+                        "${key.replaceAll(/\W/, '_')}""${value.replaceAll(/^\s+|\s+$/, '')}"
+                }
+        }
+
+    if (csvDebug) {println "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"+xmlString}
+
     def xmlContent = XMLUtils.fromString("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"+xmlString)
-	store.add(new XmlRecord(xmlContent, config.value("csv.sourceurl")+"/doc"+i))
-	i++
+        store.add(new XmlRecord(xmlContent, config.value("csv.sourceurl")+"/doc"+i))
+        i++
 }
 
 // close() required for the store to be flushed
