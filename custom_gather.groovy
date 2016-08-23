a16080609c7ac25a1c52f11d5427f598263543b8
Peter Levan <plevan@peter-levans-macbook-pro.local>
2016-08-23 14:29:32 +1000

    PL: changed to read multiple CSVs from start urls file

--- custom_gather.groovy
+++ custom_gather.groovy
@@ -12,9 +12,8 @@ import static org.apache.commons.csv.CSVFormat.*
 // Create a configuration object to read collection.cfg
 def config = new NoOptionsConfig(new File(args[0]), args[1]);
 
-if (config.value("csv.sourceurl") == null) {
-        throw new RuntimeException("Error: CSV source URL is not defined");
-}
+// Read $SEARCH_HOME
+def searchHome = Environment.getValidSearchHome().getCanonicalPath();
 
 // Create a Store instance to store gathered data
 def store = new XmlStoreFactory(config).newStore();
@@ -37,55 +36,69 @@ if (config.value("csv.debug") != null) {csvDebug = config.value("csv.debug")}
 // Open the XML store
 store.open()
 
-// Fetch the CSV file and convert it to XML
-def csvText = new URL(config.value("csv.sourceurl")).getText(csvEncoding)
-// remove blank lines
-csvText = csvText.replaceAll("[\n\r]+","\n")
+// Open the start URLs file
+File file = new File(searchHome+File.separatorChar+"conf"+File.separatorChar+config.value("collection")+File.separatorChar+"collection.cfg.start.urls")
 
-if (csvDebug) {println csvText}
 
-CSVParser csv = null
-if (Boolean.parseBoolean(csvHeader)) {
-    // use the header row to define fields
-        csv = CSVParser.parse(csvText, csvFormat[format].withHeader())
+def line
+file.withReader { reader ->
+    while ((line = reader.readLine())!=null) {
+
+        println "Gathering CSV for "+line;
+
+		// Fetch the CSV file and convert it to XML
+		def csvText = new URL(line).getText(csvEncoding)
+		// remove blank lines
+		csvText = csvText.replaceAll("[\n\r]+","\n")
+
+		if (csvDebug) {println csvText}
+
+		CSVParser csv = null
+		if (Boolean.parseBoolean(csvHeader)) {
+		    // use the header row to define fields
+		        csv = CSVParser.parse(csvText, csvFormat[format].withHeader())
+		}
+		else {
+		        if (config.value("csv.header.custom") != null) {
+		        // use field definitions
+		                csv = CSVParser.parse(csvText, csvFormat[format].withHeader(config.value("csv.header.custom").split(",")))
+		        }
+		        else {
+		                csv = CSVParser.parse(csvText, csvFormat[format])
+		        }
+		}
+		
+		//Note: counter will reset for each file
+		def i=0;
+
+		for (record in csv.iterator()) {
+		    if ((i % 100) == 0)
+		    {
+		    	// Check to see if the update has been stopped 
+		        if (config.isUpdateStopped()) {
+		                store.close()
+		                throw new RuntimeException("Update stop requested by user.");
+		        }    	
+		        config.setProgressMessage("Processed "+i+" records");
+		    }
+		    def fields = record.toMap();
+		        def xmlString = new StringWriter();
+		        def xmlRecord = new groovy.xml.MarkupBuilder(xmlString);
+
+		        xmlRecord.item() {
+		                fields.each() {key, value ->
+		                        // replace non-word chars with _ to avoid illegal XML field names and strip whitespace from values
+		                        "${key.replaceAll(/\W/, '_')}""${value.replaceAll(/^\s+|\s+$/, '')}"
+		                }
+		        }
+
+		    if (csvDebug) {println "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"+xmlString}
+
+		    def xmlContent = XMLUtils.fromString("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"+xmlString)
+		        store.add(new XmlRecord(xmlContent, line+"/doc"+i))
+		        i++
+		}
+	}
 }
-else {
-        if (config.value("csv.header.custom") != null) {
-        // use field definitions
-                csv = CSVParser.parse(csvText, csvFormat[format].withHeader(config.value("csv.header.custom").split(",")))
-        }
-        else {
-                csv = CSVParser.parse(csvText, csvFormat[format])
-        }
-}
-def i=0;
-for (record in csv.iterator()) {
-    if ((i % 100) == 0)
-    {
-    	// Check to see if the update has been stopped 
-        if (config.isUpdateStopped()) {
-                store.close()
-                throw new RuntimeException("Update stop requested by user.");
-        }    	
-        config.setProgressMessage("Processed "+i+" records");
-    }
-    def fields = record.toMap();
-        def xmlString = new StringWriter();
-        def xmlRecord = new groovy.xml.MarkupBuilder(xmlString);
-
-        xmlRecord.item() {
-                fields.each() {key, value ->
-                        // replace non-word chars with _ to avoid illegal XML field names and strip whitespace from values
-                        "${key.replaceAll(/\W/, '_')}""${value.replaceAll(/^\s+|\s+$/, '')}"
-                }
-        }
-
-    if (csvDebug) {println "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"+xmlString}
-
-    def xmlContent = XMLUtils.fromString("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"+xmlString)
-        store.add(new XmlRecord(xmlContent, config.value("csv.sourceurl")+"/doc"+i))
-        i++
-}
-
 // close() required for the store to be flushed
 store.close()
